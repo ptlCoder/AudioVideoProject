788c070bc92fabc78648c886a568627690402d60
ptlCoder <pengtanglong_local@163.com>
2018-05-16 15:09:44 +0800

    添加 iOS系统音效demo

--- iOS系统音效/iOS系统音效/ViewController.m
+++ iOS系统音效/iOS系统音效/ViewController.m
@@ -7,6 +7,7 @@
 //
 
 #import "ViewController.h"
+#import <AudioToolbox/AudioToolbox.h>
 
 @interface ViewController ()
 
@@ -16,14 +17,31 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+
 }
 
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+void AudioServicesSystemSoundCompletion(SystemSoundID ssID,void* __nullable    clientData) {
+    NSLog(@"播放完成---");
 }
+
+-(void)touchesBegan:(NSSet<UITouch *> *)touches withEvent:(UIEvent *)event {
+        NSString *audioFile = [[NSBundle mainBundle]pathForResource:@"call.caf" ofType:nil];
+        NSURL *fileUrl = [NSURL fileURLWithPath:audioFile];
+        // 1.获得系统声音ID
+        SystemSoundID soundID = 0;
+        /**
+         * inFileUrl:音频文件url
+         * outSystemSoundID:声音id（此函数会将音效文件加入到系统音频服务中并返回一个长整形ID）
+         */
+        AudioServicesCreateSystemSoundID((__bridge CFURLRef _Nonnull)(fileUrl), &soundID);
+        //如果需要在播放完之后执行某些操作，可以调用如下方法注册一个播放完成回调函数
+        AudioServicesAddSystemSoundCompletion(soundID, NULL, NULL, AudioServicesSystemSoundCompletion, NULL);
+        // 2.播放音频
+         AudioServicesPlaySystemSound(soundID);//播放音效
+    //    AudioServicesPlayAlertSound(soundID);//播放音效并震动
+}
+
 
 
 @end
---
+++ iOS音乐播放/iOS音乐播放/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  iOS音乐播放
+//
+//  Created by soliloquy on 2018/5/16.
+//  Copyright © 2018年 soliloquy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
